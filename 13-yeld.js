0b87e42c890f695a034e6cedbc4da6ac10faf843
Valerio Radice <valix85@gmail.com>
2019-04-16 21:37:45 +0200

    update example

--- 13-yeld.js
+++
@@ -1,1 +0,0 @@
-//For use this file install quokka.js in visual studio code and enable it on this file.
---
+++ 13-yield.js
@@ -0,0 +1,7 @@
+//For use this file install quokka.js in visual studio code and enable it on this file.
+
+//create a generator function, use * symbol after function
+
+function* myGen(){
+    yield
+}
